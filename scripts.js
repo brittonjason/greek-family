19cbadf35a7edf22c3ac09f140d34430121d3bec
bentham12 <bjason1@vt.edu>
2018-02-18 17:43:48 -0500

    Added subject page

---
+++ scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+    function openTab(button, contentName) {
+        $('.tabcontent').hide();
+        $('.tablinks active').attr('class', 'tablinks');
+        $(contentName).show();
+        button.attr('class', 'tablinks active');
+    }
+
+    $('#parentsButton').click(function() {
+        openTab($(this), '#Parents')
+    });
+
+    $('#consortButton').click(function() {
+        openTab($(this), '#Consort')
+    });
+
+    $('#childrenButton').click(function() {
+        openTab($(this), '#Children')
+    });
+
+    $('#siblingsButton').click(function() {
+        openTab($(this), '#Siblings')
+    });
+
+
+});
